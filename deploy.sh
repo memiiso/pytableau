717509810748fde9f5a1ae2aa321eb1d303060db
Ismail Simsek <ismail.simsek@ppro.com>
2020-07-16 18:59:00 +0200

    added github action

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 # build and test
 python3 --version
+python3 -m pip install coverage pylint pytest
 python3 -m pylint pytableau setup.py
 python3 -m compileall -f pytableau setup.py
 python3 -m coverage report -m ./pytableau/*.py
-python3 -m coverage run --source=./tests/ -m unittest discover -s tests/
 python3 setup.py -q install --user --force
